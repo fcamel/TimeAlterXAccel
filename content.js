ed9f482f4b9c0d5cfddb5a19ac024d5243455d08
fcamel <fcamel@gmail.com>
2017-08-20 20:17:40 +0800

    Show the dialog after updating the playback rate.

--- content.js
+++ content.js
@@ -5,7 +5,49 @@ chrome.runtime.onMessage.addListener(
                 "from a content script:" + sender.tab.url :
                 "from the extension: rate=" + request.rate);
     if (!sender.tab && request.rate) {
+        // Change the playback rate.
         document.querySelector('video').playbackRate = request.rate;
+
+        // Show a dialog.
+        let id = '__time_alter_x_accel_dialog__';
+        let e = document.createElement('div')
+        e.id = id;
+        let width = 500;
+        let height = 200;
+        e.style.position = 'fixed'
+        e.style.zIndex = 99999;
+        e.style.width = '' + width + 'px';
+        e.style.height ='' + height + 'px';
+        e.style.left = '' + (window.innerWidth - width) / 2 + 'px';
+        e.style.top = '' + (window.innerHeight - height) / 3 + 'px';
+        e.style.color = '#000';
+        e.style.padding = '30px';
+        e.style.backgroundColor = '#fff';
+        e.style.border = '2px solid gray';
+        e.style.fontSize = '30px';
+        e.style.borderRadius = '15px';
+        // Align both sides to center.
+        e.style.textAlign = 'center';
+        e.style.lineHeight = '' + height + 'px';
+
+        e.innerText = 'Update the playback rate to ' + request.rate;
+
+        document.body.appendChild(e);
+
+        // Hide the dialog.
+        setTimeout(function() {
+          // Fade out.
+          e.style.transition = 'visibility 0s 0.5s, opacity 0.5s linear';
+          e.style.visibility = 'hidden';
+          e.style.opacity = 0;
+        }, 1500);
+
+        e.addEventListener('webkitTransitionEnd', function() {
+          let t = document.getElementById(id);
+          if (t) {
+            document.body.removeChild(t);
+          }
+        }, false);
     }
   }
 );
